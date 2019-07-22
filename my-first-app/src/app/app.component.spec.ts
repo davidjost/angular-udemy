da75626989f7abe636328bb50f1842bfe12e563d
davidjost <david.jost@rewe-digital.com>
2019-07-22 11:38:54 +0200

    updated structure

--- my-first-app/src/app/app.component.spec.ts
+++ my-first-app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-udemy'`, () => {
+  it(`should have as title 'my-first-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-udemy');
+    expect(app.title).toEqual('my-first-app');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-udemy!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to my-first-app!');
   });
 });
