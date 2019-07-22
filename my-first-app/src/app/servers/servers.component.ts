82ba9911d827c828a66eb44774a135264f4c6c80
davidjost <david.jost@rewe-digital.com>
2019-07-22 15:36:36 +0200

    solved assignment 2

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TargetLocator } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-servers',
@@ -7,14 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false
+  allowNewUser = false
+  serverCreationStatus = 'No server was created'
+  userCreationStatus = 'No user was created'
+  serverName = 'DEFAULT'
+  userName = ''
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true
-    }, 2000);
+    }, 1000);
    }
 
   ngOnInit() {
+  }
+
+  allowUserAdding(event) {
+    if (event.target.value !== '') {
+      this.allowNewUser = true
+    } else {
+      this.allowNewUser = false
+    }
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'server ' + this.serverName + ' was created!'
+  }
+
+  onCreateUser() {
+    this.userCreationStatus = 'user ' + this.userName + ' was created!'
+    this.userName = ''
+    this.allowNewUser = false
+  }
+
+  onUpdateServerName(event) {
+    // this method is only needed in 1-way data binding
+    this.serverName = event.target.value
   }
 
 }
