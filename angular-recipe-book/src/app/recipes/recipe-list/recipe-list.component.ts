40961fc0cc30335a9a3bc1125784e4496d102c86
davidjost <david.jost@rewe-digital.com>
2019-07-24 10:29:32 +0200

    updated a whole lot of stuff

---
+++ angular-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    // tslint:disable-next-line: max-line-length
+    new Recipe('A test Recipe', 'this is simply a test', 'https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1895&q=80'),
+    new Recipe('A test Recipe', 'this is simply a test', 'https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1895&q=80')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
